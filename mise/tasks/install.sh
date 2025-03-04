bfed614e2389fcc19f0813fd2217ccfb02e2a839
Pedro <pedro@pepicrft.me>
2025-03-04 18:10:49 +0100

    Make scripts fail if any of the scripts in them fail

--- mise/tasks/install.sh
+++ mise/tasks/install.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 #MISE description="Install the project dependencies"
 
+set -eo pipefail
+
 pnpm install
 mkdir -p bin/ && cp $(which tuist) bin/
--- mise/tasks/test.sh
+++ mise/tasks/test.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 #MISE description="Test the project"
 
+set -eo pipefail
+
 node --test "src/**/*.test.js"
